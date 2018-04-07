16ca70badd6ef770f0d48cfad741a3621b4dd523
N Gause <35553811+ngause@users.noreply.github.com>
2018-04-07 12:08:10 -0500

    Updated buttons designs and functions. Updated string resource. Updated colors resource. Updated dimens resource. Updated activity to send order message via email

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.android.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
-
-import java.text.NumberFormat;
+import android.widget.Toast;
 
 /**
  * This app displays an order form to order coffee.
@@ -13,8 +16,10 @@ import java.text.NumberFormat;
 public class MainActivity extends AppCompatActivity {
 
     int quantity = 0;
-    int pricePerCup = 5;
-    int price = 0;
+    //int pricePerCup = 5;
+    //int price = 0;
+    int whipCreamCost = 1;
+    int chocCost = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,38 +27,109 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+
+    //https://gist.github.com/udacityandroid/609dbb5370ba0665955a
+    /**
+     * Calculates the price of the order based on the current quantity.
+     *
+     * @return the price
+     */
+    private int calculatePrice(boolean hasWhippedCream, boolean hasChocolate) {
+        int pricePerCup = 5;
+        if (hasWhippedCream == true) {
+            pricePerCup += whipCreamCost;
+        }
+        if (hasChocolate == true) {
+            pricePerCup += chocCost;
+        }
+        return quantity * pricePerCup;
+        //pricePerCup=5;
+        //return price;
+    }
+
+    private String createOrderSummary(int price, boolean hasWhippedCream, boolean hasChocolate) {
+        //OLD EditText orderName = (EditText) findViewById(R.id.order_name);
+        //OLD String orderNameStng = orderName.getText().toString();
+        //OLD calculatePrice(hasWhippedCream, hasChocolate);
+
+        String orderName = ((EditText) findViewById(R.id.order_name)).getText().toString();
+        String priceMessage = "Name: " + orderName;
+        if (hasWhippedCream == true) {
+            priceMessage += "\nHas whipped cream?: Yes";
+        } else if (hasWhippedCream == false) {
+            priceMessage += "\nHas whipped cream?: No";
+        }
+        if (hasChocolate == true) {
+            priceMessage += "\nHas whipped cream?: Yes";
+        } else if (hasChocolate == false) {
+            priceMessage += "\nHas whipped cream?: No";
+        }
+        priceMessage += "\nQuantity: " + quantity;
+        priceMessage += "\nTotal: " + price;
+        priceMessage += "\nThank you!";
+        return priceMessage;
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+//    private void displayMessage(String message) {
+//        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+//        orderSummaryTextView.setText(message);
+//    }
+
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        price = pricePerCup * quantity;
-        //displayPrice(price);
-        String priceMessage = "Total: $" + price + "\nThank You!";
-        displayMessage(priceMessage);
+        CheckBox checkBoxViewWhip = (CheckBox) findViewById(R.id.whipped_cream);
+        boolean hasWhippedCream = checkBoxViewWhip.isChecked();
+        CheckBox checkBoxViewChoc = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        boolean hasChocolate = checkBoxViewChoc.isChecked();
+
+        int price = calculatePrice(hasWhippedCream, hasChocolate);
+        //OLD displayMessage(createOrderSummary(price, hasWhippedCream, hasChocolate));
+
+        String emailMsg = createOrderSummary(price, hasWhippedCream, hasChocolate);
+        String orderName = ((EditText) findViewById(R.id.order_name)).getText().toString();
+        String emailSubject = "JustJava order for " + orderName;
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        //intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, emailSubject);
+        intent.putExtra(Intent.EXTRA_TEXT, emailMsg);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
+//        Intent intent = new Intent(Intent.ACTION_VIEW);
+//        intent.setData(Uri.parse("geo:29.475587, -95.514693"));
+//        if (intent.resolveActivity(getPackageManager()) != null) {
+//            startActivity(intent);
+//        }
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
-    }
-
-    /**
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 
     /**
      * This method is called when the plus button is is clicked.
      */
     public void increment(View view) {
-        quantity = quantity +1;
-        display(quantity);
+        if (quantity < 100) {
+            quantity += 1;
+        } else {
+            CharSequence tooMuchCoffee = "You cannot order more than 100 cups of coffee";
+            int duration = Toast.LENGTH_SHORT;
+            Toast tooMuchToast = Toast.makeText(getApplicationContext(), tooMuchCoffee, duration);
+            tooMuchToast.show();
+        }
+        displayQuantity(quantity);
     }
 
     /**
@@ -62,16 +138,14 @@ public class MainActivity extends AppCompatActivity {
     public void decrement(View view) {
         if (quantity > 0) {
             quantity = quantity - 1;
+        } else {
+            //Context context = getApplicationContext();
+            CharSequence tooLittleCoffee = "You cannot have less than 0 cups of coffee";
+            int duration = Toast.LENGTH_SHORT;
+            Toast tooLittleToast = Toast.makeText(getApplicationContext(), tooLittleCoffee, duration);
+            tooLittleToast.show();
         }
-        display(quantity);
-    }
-
-    /**
-     * This method displays the given text on the screen.
-     */
-    private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        displayQuantity(quantity);
     }
 
 }
